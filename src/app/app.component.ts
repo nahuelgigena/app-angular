c510a76013b447e2e1eb8fc357045ecbc8694fb1
nahuelgigena <nahuelgigena@outlook.com>
2021-03-03 15:17:06 -0300

    V-1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,5 +21,15 @@ export class AppComponent {
       new Producto( 'Hamburguesa','https://www.profesionalhoreca.com/wp-content/uploads/2020/02/profesionalhoreca-The-Fitzgerald-Burger-Company-1.jpg', 350),
       new Producto( 'Hamburguesa','https://www.profesionalhoreca.com/wp-content/uploads/2020/02/profesionalhoreca-The-Fitzgerald-Burger-Company-1.jpg', 350),
     ]
+    
+    this.arrBebida= [
+      new Producto( 'Gaseosa','https://s03.s3c.es/imag/_v0/770x420/5/3/f/600x400_Latas-de-Coca-Cola-y-Pepsi.jpg', 350),
+      new Producto( 'Hamburguesa','https://s03.s3c.es/imag/_v0/770x420/5/3/f/600x400_Latas-de-Coca-Cola-y-Pepsi.jpg', 350),
+      new Producto( 'Hamburguesa','https://s03.s3c.es/imag/_v0/770x420/5/3/f/600x400_Latas-de-Coca-Cola-y-Pepsi.jpg', 350),
+      new Producto( 'Hamburguesa','https://s03.s3c.es/imag/_v0/770x420/5/3/f/600x400_Latas-de-Coca-Cola-y-Pepsi.jpg', 350),
+      new Producto( 'Hamburguesa','https://s03.s3c.es/imag/_v0/770x420/5/3/f/600x400_Latas-de-Coca-Cola-y-Pepsi.jpg', 350),
+
+    ]
+    
   }
 }
